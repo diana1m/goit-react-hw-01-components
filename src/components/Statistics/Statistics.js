6f1dc0d78070416751bd34d908611f6366ddd534
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-02-18 21:41:10 +0200

    bugfix getrandomcolor

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -27,5 +27,7 @@ Statistics.propTypes = {
 }
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
   }
