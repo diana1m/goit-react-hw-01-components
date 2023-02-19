ae6eeb60f309dbf184529187833dc455709cc4a8
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-02-19 15:46:38 +0200

    Create data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import user from '../Data/user.json';
+import data from '../Data/data.json';
+import friends from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 import { Profile } from './Profile/Profile';
 import { GlobalStyle } from 'GlobalStyle';
 import { Container } from './Layout/Layout.styled';
